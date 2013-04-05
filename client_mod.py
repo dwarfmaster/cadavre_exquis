f3447bafb6ec601f5d104a83d0aefd23f2a25754
lucas8 <luc.linux@mailoo.org>
2013-04-05 19:08:36 +0200

    Client minimal fonctionnel.

--- client_mod.py
+++ client_mod.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
+import socket
 
+cnx = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cnx.connect(("localhost", 7777))
+
+part = cnx.recv(1024).decode("UTF-8")
+print("Veuillez entrer le \"", part, "\" :", sep="", end=" ")
+phr = input()
+cnx.send(bytes(phr, "UTF-8"))
+
+phr = cnx.recv(1024).decode("UTF-8")
+print("La phrase est : \"", phr, "\".", sep="")
+
+cnx.close()
 
 
