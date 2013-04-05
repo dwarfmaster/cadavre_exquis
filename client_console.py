fd14828424355b6fc70dc80b45a7f31be5fc499a
lucas8 <luc.linux@mailoo.org>
2013-04-05 19:42:48 +0200

    Création d'un client en console.

--- client_console.py
+++ client_console.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
+import client_mod as lce
+
+host = input("Entrez l'adresse du serveur :")
+lce.connect(host)
+
+print("Veuillez entrer le \"", lce.getPart(), "\" :", sep="", end=" ")
+lce.sendPart(input())
+print("La phrase est : \"", lce.end(), "\".", sep="")
 
 
 
